9794ae8ed805e0fcd34984e86348ddfa79e2c366
remtav <34774759+remtav@users.noreply.github.com>
2021-10-15 23:11:56 -0400

    bingo_prep_lbls.py: clean up more detect.py: add confidence to inference output

--- bingo_prep_lbls.py
+++ bingo_prep_lbls.py
@@ -8,7 +8,7 @@ import cv2
 import torch
 from tqdm import tqdm
 
-csv_file = Path('/home/remi/Documents/sherbrooke_citoyen/training_img_14oct/bingo_trn.csv')
+csv_file = Path('/home/remi/Documents/sherbrooke_citoyen/training_img_15oct/bingo_trn.csv')
 out_root = Path('/media/data/bingo')
 val_perc = 25  # percentage of training data going to validation rather than training
 random.seed(1234)
@@ -32,34 +32,36 @@ with open(csv_file, 'r') as f:
         out_gt_dir_dataset.mkdir(exist_ok=True)
 
         # read info from csv starting with file path
-        row_splits = row[0].split('.png')
-        file = Path(f"{row_splits[0]}.png")  # add ".png" is a work around because csv is not well written
+        file = Path(f"{row[0]}")  # add ".png" is a work around because csv is not well written
         # check file's existence
         if not file.is_file():
             raise FileNotFoundError(f'Not found: {file}')
         # copy to destination folder
         if not (out_img_dir_dataset / file.name).is_file():
-            shutil.copy(file, out_img_dir)
-            print(f'Copied: {file}')
+            shutil.copy(file, out_img_dir_dataset)
+            #print(f'Copied: {file}')
         # start preparing label files
         img = cv2.imread(str(file))
         # print(img.shape)
         out_lbl_lines = []
         lbl_file = out_gt_dir_dataset / f'{file.stem}.txt'
         if not lbl_file.is_file():
-            with open(out_gt_dir_dataset / f'{file.stem}.txt', 'w') as out_lbl:
-                for bbox in eval(row_splits[1]):
-                    # make bboxes relative to image shape and place xy's in center of box, not upper right corner
-                    # (see coco dataset convention for xywh labeling)
-                    x_rel = (bbox[0] + bbox[2]/2) / img.shape[1]
-                    y_rel = (bbox[1] + bbox[3]/2) / img.shape[0]
-                    w_rel = bbox[2] / img.shape[1]
-                    h_rel = bbox[3] / img.shape[0]
-                    xywh_rel = f'0 {x_rel} {y_rel} {w_rel} {h_rel}\n'
-                    out_lbl_lines.append(xywh_rel)
-                    # print(bbox)
-                    # print(xywh_rel)
-                    #cv2.rectangle(img, (int((x_rel-w_rel/2)*img.shape[1]), int((y_rel-h_rel/2)*img.shape[0])), (int((x_rel+w_rel/2)*img.shape[1]), int((y_rel+h_rel/2)*img.shape[0])), (0, 255, 0), 2)
+            with open(str(lbl_file), 'w') as out_lbl:
+                if row[1] is not '':
+                    for bbox in eval(row[1]):
+                        # make bboxes relative to image shape and place xy's in center of box, not upper right corner
+                        # (see coco dataset convention for xywh labeling)
+                        x_rel = (bbox[0] + bbox[2]/2) / img.shape[1]
+                        y_rel = (bbox[1] + bbox[3]/2) / img.shape[0]
+                        w_rel = bbox[2] / img.shape[1]
+                        h_rel = bbox[3] / img.shape[0]
+                        xywh_rel = f'0 {x_rel} {y_rel} {w_rel} {h_rel}\n'
+                        out_lbl_lines.append(xywh_rel)
+                        # print(bbox)
+                        # print(xywh_rel)
+                        #cv2.rectangle(img, (int((x_rel-w_rel/2)*img.shape[1]), int((y_rel-h_rel/2)*img.shape[0])), (int((x_rel+w_rel/2)*img.shape[1]), int((y_rel+h_rel/2)*img.shape[0])), (0, 255, 0), 2)
+                else:
+                    continue
                 # for debugging
                 # cv2.imshow('img', img)
                 # cv2.waitKey()
